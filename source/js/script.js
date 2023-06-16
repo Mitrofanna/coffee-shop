90f4170585c680db84cd713cd3aadde19f95721d
Anastasia Mitrofanova <mitrofanovaa70@gmail.com>
2023-06-17 00:21:07 +0300

    Реализует закрытие/открытие мобильного меню

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,14 @@
-/* в этот файл добавляет скрипты*/
+const nav = document.querySelector('.navigation');
+const navToggle = document.querySelector('.navigation__button');
+
+nav.classList.add('navigation--close');
+
+navToggle.addEventListener('click', function () {
+  if (nav.classList.contains('navigation--close')) {
+    nav.classList.remove('navigation--close');
+    nav.classList.add('navigation--open');
+  } else {
+    nav.classList.add('navigation--close');
+    nav.classList.remove('navigation--open');
+  }
+});
