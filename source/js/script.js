5aecdd92921df6a5e725fc75931e180e2826c39d
Anastasia Mitrofanova <mitrofanovaa70@gmail.com>
2023-06-17 04:33:40 +0300

    Стилизует радиокнопки

--- source/js/script.js
+++ source/js/script.js
@@ -12,3 +12,24 @@ navToggle.addEventListener('click', function () {
     nav.classList.remove('navigation--open');
   }
 });
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
