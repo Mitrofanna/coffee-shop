49507b79d80dca8a8400037b3cd0f43f1e1d2474
Anastasia Mitrofanova <mitrofanovaa70@gmail.com>
2023-06-18 23:46:39 +0300

    Подключает nouislider и добавляет стили

--- source/js/script.js
+++ source/js/script.js
@@ -13,11 +13,11 @@ navToggle.addEventListener('click', function () {
   }
 });
 
+//Слайдер для галереи
 const swiper = new Swiper('.swiper', {
   direction: 'horizontal',
   loop: true,
 
-  // If we need pagination
   pagination: {
     el: '.swiper-pagination',
     type: 'bullets',
@@ -31,3 +31,38 @@ const swiper = new Swiper('.swiper', {
 
 });
 
+//Рэндж слайдер для цены
+const rangeSlider = document.getElementById('range-slider');
+
+if (rangeSlider) {
+	noUiSlider.create(rangeSlider, {
+    start: [0, 900],
+		connect: true,
+		step: 1,
+    range: {
+			'min': [0],
+			'max': [1000]
+    }
+	});
+
+	const inputMin = document.getElementById('input-min');
+	const inputMax = document.getElementById('input-max');
+	const inputs = [inputMin, inputMax];
+
+	rangeSlider.noUiSlider.on('update', function(values, handle){
+		inputs[handle].value = Math.round(values[handle]);
+	});
+
+	const setRangeSlider = (i, value) => {
+		let arr = [null, null];
+		arr[i] = value;
+
+		rangeSlider.noUiSlider.set(arr);
+	};
+
+	inputs.forEach((el, index) => {
+		el.addEventListener('change', (e) => {
+			setRangeSlider(index, e.currentTarget.value);
+		});
+	});
+}
